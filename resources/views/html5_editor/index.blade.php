b135ec69688938c53dbfa6791c05983dc22e3169
Rahul <rahulchoudhary666666@gmail.com>
2020-10-05 16:51:56 +0530

    html editor fix

--- resources/views/html5_editor/index.blade.php
+++ resources/views/html5_editor/index.blade.php
@@ -233,7 +233,7 @@
                 // When the user clicks anywhere outside of the modal, close it
                 window.onclick = function(event) {
                     if (event.target == modal) {
-                        modal.style.display = "none";
+                        //modal.style.display = "none";
                     }
                 }
                 // When the user clicks on closeBtn, close the modal
